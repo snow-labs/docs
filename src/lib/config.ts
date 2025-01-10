c394ec57a5dab067abc6372a41083d6d6b14b2c8
AmethystDevbyFK <connorhartey16@icloud.com>
2025-01-10 16:35:13 +0000

    Fixed the website and made it actually good

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,6 +1,6 @@
 export default {
     title: 'Snow',
-    discord: '/discord',
+    discord: 'https://discord.gg/adqQ7Qyr79',
     github: 'https://github.com/snow-labs/make-docs',
     developer: 'https://amethystdev.com',
     description: 'A modern, open-source static-site generator, for making docs websites, using SvelteKit, Skeleton and TailwindCSS.',
